35f2697ed9fef49f1479d3d94816fc69cc019e25
rtmtdrv <art.todorov@me.com>
2019-09-12 14:13:28 -0700

    position hell

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55200" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58758" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
