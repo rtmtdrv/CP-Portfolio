ccf4bad4ae2d0b942db6bd286640d32ca514c4d0
rtmtdrv <art.todorov@me.com>
2019-09-20 21:07:32 -0700

    added animations for projects

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -154,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64366" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55739" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
